a35f84074153bad79c3e0da91267003638773d7c
steniogobira <steniogobira@gmail.com>
2023-01-04 00:00:06 -0300

    Correções de Bugs e novas implementações de função

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ print('='*60)
 print('Olá, você está na Axiom locadora de carros'.center(60))
 print('='*60)
 nome = input('Por favor, digite seu nome: ')
+
 portifolio = {'Chevrolet Tracker': 120, 'Chevrolet Onix': 90, 'Chevrolet Spin': 150,
               'Hyundai HB20': 85, 'Hyndai Tucson': 120, 'Fiat Uno': 60, 'Fiat Mobi': 70, 'Fiat Pulse': 130}
 carros_alugados = {}
+
+carros_lista = list(portifolio.keys())
+valores_lista = list(portifolio.values())
+
+
+def mostrar_carros(dicionario):
+    i = 0
+    for carro, valor in dicionario.items():
+        print(f'[{i}] - {carro} R${valor}/dia')
+        i += 1
 
 
 while True:
@@ -21,21 +32,15 @@ while True:
     if operacao == 0:
         print('[PORTIFÓLIO] Esse é o  nosso portifólio.')
         print('')
-        i = 0
-        for carro, valor in portifolio.items():
-            print(f'[{i}] - {carro} R${valor}/dia')
-            i += 1
+        mostrar_carros(portifolio)
 
     elif operacao == 1:
         print('='*60)
         print('[ALUGUEL] Escolha o código de algum de nossos carros disponíveis:')
-
-        for carro, valor in portifolio.items():
-            print(f'[{i}] - {carro} R${valor}/dia')
-            i += 1
+        mostrar_carros(portifolio)
 
         codigo_carro = int(input())
-        while codigo_carro not in [0, 1, 2, 3, 4, 5, 6, 7, 8]:
+        while codigo_carro not in range(0, ((len(carros_lista)) + 1)):
             print('Por favor digite alguma das opções do Menu!')
             codigo_carro = int(input())
 
@@ -44,9 +49,11 @@ while True:
 
         print('='*60)
         print(
-            f'Você escolheu {list(portifolio.keys())[codigo_carro]} por {dias} dias')
-        valor_aluguel = (list(portifolio.values())[codigo_carro]) * dias
+            f'Você escolheu {carros_lista[codigo_carro]} por {dias} dias'
+        )
+        valor_aluguel = (valores_lista[codigo_carro]) * dias
         print(f'O aluguel totalizaria R${valor_aluguel}. Deseja alugar?')
+
         print('='*60)
         print('0 - SIM | 1 - NÃO')
         comando = int(input())
@@ -57,27 +64,38 @@ while True:
         if comando == 0:
             print('='*60)
             print(
-                f'Parabéns! Você alugou o {list(portifolio.keys())[codigo_carro]}'
+                f'Parabéns! Você alugou o {carros_lista[codigo_carro]}'
             )
-            carros_alugados[list(portifolio.keys())[codigo_carro]] = list(
-                portifolio.values())[codigo_carro]
-            portifolio.pop(list(portifolio.keys())[codigo_carro])
+            carros_alugados[carros_lista[codigo_carro]
+                            ] = valores_lista[codigo_carro]
+
+            carros_alugados_lista = list(carros_alugados.keys())
+            valores_carros_alugados_lista = list(carros_alugados.values())
+
+            portifolio.pop(carros_lista[codigo_carro])
 
     elif operacao == 2:
-        print('='*60)
-        print('[DEVOLUÇÃO] Segue a lista de carros alugados, qual você deseja devolver?')
-        i = 0
-        for carro, valor in carros_alugados.items():
-            print(f'[{i}] - {carro} R${valor}/dia')
-            i += 1
-        print('='*60)
-        print('Escolha o código do carro que deseja devolver:')
-        devolvercarro = int(input())
-        print(
-            f'Obrigado por devolver o carro {list(carros_alugados.keys())[devolvercarro]}')
-        portifolio[list(carros_alugados.keys())[devolvercarro]] = list(
-            carros_alugados.values())[devolvercarro]
-        carros_alugados.pop(list(carros_alugados.keys())[devolvercarro])
+        if (len(carros_alugados)) == 0:
+            print("Não existem carros alugados")
+        else:
+            print('='*60)
+            print('[DEVOLUÇÃO] Segue a lista de carros alugados, qual você deseja devolver?'
+                  )
+            mostrar_carros(carros_alugados)
+
+            print('='*60)
+            print('Escolha o código do carro que deseja devolver:')
+            devolvercarro = int(input())
+            while devolvercarro not in range(0, ((len(carros_alugados_lista)) + 1)):
+                print('Por favor digite alguma das opções do Menu!')
+                devolvercarro = int(input())
+
+            print(
+                f'Obrigado por devolver o carro {carros_alugados_lista[devolvercarro]}'
+            )
+            portifolio[carros_alugados_lista[devolvercarro]
+                       ] = valores_carros_alugados_lista[devolvercarro]
+            carros_alugados.pop(carros_alugados_lista[devolvercarro])
 
     print('='*60)
     print('0 - CONTINUAR | 1 - SAIR')
